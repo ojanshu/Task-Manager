3a5fb3930d9295ef7e886921c96699cbca99bac2
Ojanshu <ojanshu.tripathi@gmail.com>
2023-12-09 17:14:48 +0530

    final files

---
+++ middleware/error-handler.js
@@ -0,0 +1,9 @@
+const { CustomAPIError } = require("../errors/custom-error")
+const errorHandlerMiddleware = (err,rew,res,next) => {
+    if(err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json( {msg: err.message} )
+    }
+    res.status(500).json( {msg: "Maintainance mode on"} )
+}
+
+module.exports = errorHandlerMiddleware
