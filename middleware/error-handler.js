4eb3c3512c71ac9a9adbc0a376a9cd3cc6a88c95
Ojanshu <ojanshu.tripathi@gmail.com>
2024-01-21 11:37:46 +0530

    updated error-handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const { CustomAPIError } = require("../errors/custom-error")
-const errorHandlerMiddleware = (err,rew,res,next) => {
+const errorHandlerMiddleware = (err,req,res,next) => {
     if(err instanceof CustomAPIError) {
         return res.status(err.statusCode).json( {msg: err.message} )
     }
